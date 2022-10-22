d55777c9e9a943c2ee454256a2e44286c6324674
Kaique de Miranda <kbdemiranda@hotmail.com>
2022-10-21 21:42:04 -0300

    Iniciando o Projeto

---
+++ src/main/java/io/github/noazul/NoAzulApplication.java
@@ -0,0 +1,13 @@
+package io.github.noazul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoAzulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoAzulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/noazul/NoAzulApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.noazul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoAzulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
