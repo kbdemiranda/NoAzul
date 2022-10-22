df189a7f12ff78a6bfb817ddab3b8e4ada3818de
Kaique de Miranda <kbdemiranda@hotmail.com>
2022-10-22 15:02:31 -0300

    Cadastro de Despesas
    -> Adição de um DTO para validação e envio informações da Despesas
    -> Criação do recurso de cadastro na classe de controller da aplicação.

--- src/main/java/io/github/noazul/resource/DespesaResource.java
+++ src/main/java/io/github/noazul/resource/DespesaResource.java
@@ -1,19 +1,23 @@
 package io.github.noazul.resource;
 
 import io.github.noazul.domain.Despesa;
+import io.github.noazul.domain.dto.DespesaDTO;
 import io.github.noazul.repository.DespesaRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/despesas")
 public class DespesaResource {
 
     private final DespesaRepository despesaRepository;
+    private final ModelMapper modelMapper = new ModelMapper();
 
     public DespesaResource(DespesaRepository despesaRepository) {
         this.despesaRepository = despesaRepository;
@@ -23,5 +27,11 @@ public class DespesaResource {
     public ResponseEntity<Page<Despesa>> getAllDespesas(Pageable pageable){
         Page<Despesa> despesas = despesaRepository.findAll(pageable);
         return ResponseEntity.ok().body(despesas);
+    }
+
+    @PostMapping
+    public ResponseEntity<Despesa> createDespesa(@RequestBody @Valid DespesaDTO dto){
+        Despesa despesa = despesaRepository.saveAndFlush(modelMapper.map(dto, Despesa.class));
+        return ResponseEntity.created(URI.create("/despesas")).body(despesa);
     }
 }
